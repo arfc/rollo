e92411b28362c7981d24401eacadb6d8b27016e7
gwenchee <gwendolynchee95@gmail.com>
2020-11-02 21:45:24 +0800

    eval and backend

--- regal/backend.py
+++ regal/backend.py
@@ -1,8 +1,28 @@
 from collections import defaultdict
+import pandas as pd
 
 class BackEnd(object):
     """ This class contains and manipulates the output results 
     """ 
 
     def __init__(self): 
-        self.results_dict = defaultdict(list)
+        self.results = defaultdict(list)
+
+    def add_ind(self, ind):
+        """ This function adds an individual's information to the results 
+        dictionary 
+        """
+        self.results['gen'].append(ind.gen)
+        self.results['ind'].append(ind.num)
+        for i, inp in enumerate(ind):
+            self.results['inp_'+str(i)] = inp
+        for i, oup in enumerate(ind.output):
+            self.results['oup_'+str(i)] = oup
+        self.export_to_txt()
+
+    def export_to_txt(self):
+        """ This function exports the current state of the results dict into 
+        an output file in the current directory as results.csv 
+        """
+        df = pd.DataFrame(data=self.results)
+        df.to_csv('results.csv')
---
+++ regal/evaluation.py
@@ -0,0 +1,26 @@
+class Evaluation(): 
+    """ This class calls various external codes 
+    
+    Attributes 
+    ----------
+    scripts = dict 
+    outputs = list of str
+        list of names of output variables to return 
+    """
+
+    def __init__(self, scripts, outputs):
+        self.openmc_script = scripts.get('openmc',False)
+        #self.openmc_outputs = outputs.get('openmc',False)
+        self.moltres_script = scripts.get('moltres',False)
+        #self.moltres_outputs = outputs.get('moltres',False)
+        ### If a user adds a new code to couple with, they should add 
+        ### it here too. 
+
+    def eval_function_generator(): 
+        """ This function returns a function that accepts a DEAP individual 
+        and returns a tuple of output values listed in outputs 
+        """
+        def eval_function():
+
+            return 
+        return eval_function
