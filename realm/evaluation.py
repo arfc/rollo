6a358467bcd602f9b3c6fcb4d67671b2e5991d64
gwenchee <gwendolynchee95@gmail.com>
2020-11-04 22:44:05 +0800

    evaluation:

--- realm/evaluation.py
+++ realm/evaluation.py
@@ -1,3 +1,5 @@
+import os, sys 
+
 class Evaluation(): 
     """ This class calls various external codes 
     
@@ -9,18 +11,25 @@ class Evaluation():
     """
 
     def __init__(self, scripts, outputs):
-        self.openmc_script = scripts.get('openmc',False)
-        #self.openmc_outputs = outputs.get('openmc',False)
-        self.moltres_script = scripts.get('moltres',False)
-        #self.moltres_outputs = outputs.get('moltres',False)
-        ### If a user adds a new code to couple with, they should add 
-        ### it here too. 
+        self.supported_codes = ['openmc', 'moltres']
+        self.all_input_map = {}
+        self.all_fitness_map = {} 
+    
+    def add_evaluator(self, code_name, input_index, fitness_index):
+        self.all_input_map[code_name] = input_index 
+        self.all_fitness_map[code_name] = fitness_index
 
     def eval_fn_generator(): 
         """ This function returns a function that accepts a DEAP individual 
         and returns a tuple of output values listed in outputs 
         """
-        def eval_function():
-            
+        def eval_function(ind):
+            for code in all_input_map:
+                path = code + '_' + str(ind.gen) + '_' + str(ind.num)
+                os.mkdir(path)
+                os.chdir(path)
+                
+
+                os.chdir('../')
             return 
         return eval_function
