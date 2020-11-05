bd2dcb878ad47da73d0073690f408c3dafa06749
gwenchee <gwendolynchee95@gmail.com>
2020-11-05 20:12:27 +0800

    eval template

--- realm/evaluation.py
+++ realm/evaluation.py
@@ -1,4 +1,5 @@
 import os, sys 
+from jinja2 import nativetypes
 
 class Evaluation(): 
     """ This class calls various external codes 
@@ -10,12 +11,16 @@ class Evaluation():
         list of names of output variables to return 
     """
 
-    def __init__(self, scripts, outputs):
+    def __init__(self):
         self.supported_codes = ['openmc', 'moltres']
+        self.scripts = {}
         self.all_input_map = {}
         self.all_fitness_map = {} 
     
-    def add_evaluator(self, code_name, input_index, fitness_index):
+    def add_evaluator(self, code_name, script, input_index, fitness_index):
+        if code_name not in supported_codes: 
+            raise Exception('REALM does not support ' + code_name)
+        self.scripts[code_name] = script
         self.all_input_map[code_name] = input_index 
         self.all_fitness_map[code_name] = fitness_index
 
@@ -24,12 +29,17 @@ class Evaluation():
         and returns a tuple of output values listed in outputs 
         """
         def eval_function(ind):
-            for code in all_input_map:
+            for code in self.scripts:
                 path = code + '_' + str(ind.gen) + '_' + str(ind.num)
                 os.mkdir(path)
                 os.chdir(path)
-                
-
+                with open(self.scripts[code], '') as file: 
+                    script = file.read() 
+                # jinja2 templating 
+                env = nativetypes.NativeEnvironment()
+                template = nativetypes.NativeTemplate(script)
+                to_run = template.render(input=ind)
+                exec(to_run)
                 os.chdir('../')
             return 
         return eval_function
