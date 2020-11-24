8dbc3188ef4ec562f4e632145ddd146ec6ccc4df
gwenchee <gwendolynchee95@gmail.com>
2020-11-24 18:37:02 +0800

    update realm

--- realm/realm.py
+++ realm/realm.py
@@ -1,6 +1,10 @@
+from .evaluation import Evaluation
+from deap import base, creator, tools, algorithms
+
 class Realm(object): 
     """ A generalized framework to generate reactor designs
     using genetic algorithms. 
     """
 
     def __init__(self): 
+
