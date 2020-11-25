f338052a0230d1b7cf90ce19cc557f9f0713fc5d
gwenchee <gwendolynchee95@gmail.com>
2020-11-25 22:47:02 +0800

    some premptive pep8

--- realm/input_validation.py
+++ realm/input_validation.py
@@ -1,80 +1,85 @@
 from jsonschema import validate
 
+
 class InputValidation():
-    """ This class does the initial validation of the *.rlm input file and 
-    and solver's templated input scripts 
+    """ This class does the initial validation of the *.rlm input file and
+    and solver's templated input scripts
     """
 
     def __init__(self, input_dict):
         self.input = input_dict
-    
-    def validate(self): 
-        """ This function validates the input dictionary and throws errors if 
-        the input file does not meet realm input file rules. 
+
+    def validate(self):
+        """ This function validates the input dictionary and throws errors if
+        the input file does not meet realm input file rules.
         """
-        print('INPUT',self.input)
+        print('INPUT', self.input)
         # validate top layer of JSON input
-        schema_top_layer= {
-            "type": "object", 
+        schema_top_layer = {
+            "type": "object",
             "properties": {
-                "control_variables": {"type": "object"}, 
-                "evaluators": {"type": "object"}, 
+                "control_variables": {"type": "object"},
+                "evaluators": {"type": "object"},
                 "constraints": {"type": "object"},
                 "algorithms": {"type": "object"}
             }
         }
         validate(instance=self.input, schema=schema_top_layer)
-        
-        # validate input variables 
+
+        # validate input variables
         try:
             input_ctrl_vars = self.input["control_variables"]
-        except KeyError as error: 
+        except KeyError as error:
             print("<Input Validation Error> At least 1 input variable must be defined.")
-        else: 
+        else:
             self.validate_ctrl_vars(input_ctrl_vars)
-        
-        # validate evaluators 
-        
-        #self.validate_evaluators() 
-        #self.validate_constraints() 
-        #self.validate_algorithms()
 
-    def validate_ctrl_vars(self, input_ctrl_vars): 
+        # validate evaluators
+
+        # self.validate_evaluators()
+        # self.validate_constraints()
+        # self.validate_algorithms()
+
+    def validate_ctrl_vars(self, input_ctrl_vars):
         """ This function validates the 'input variables' segment of the JSON
-        input file. 
+        input file.
         """
-        # special input variables with a non-conforming input style defined in 
+        # special input variables with a non-conforming input style defined in
         # input*** (add file name that has this)
-        # add to this list if a developer adds a special input variable 
+        # add to this list if a developer adds a special input variable
         special_ctrl_vars = ['polynomial']
-        
-        # validate regular input variables 
+
+        # validate regular input variables
         schema_ctrl_vars = {
-            "type": "object", 
+            "type": "object",
             "properties": {}
         }
         variables = []
         for var in input_ctrl_vars:
-            if var not in special_ctrl_vars: 
-                schema_ctrl_vars["properties"][var] = {"type": "object", 
-                                                            "properties": {
-                                                                "max": {"type": "number"}, 
-                                                                "min": {"type": "number"}
-                                                            }}
+            if var not in special_ctrl_vars:
+                schema_ctrl_vars["properties"][var] = {
+                    "type": "object", "properties": {
+                        "max": {
+                            "type": "number"}, "min": {
+                            "type": "number"}}}
                 variables.append(var)
         validate(instance=input_ctrl_vars, schema=schema_ctrl_vars)
-        for var in variables: 
-            try: 
+        for var in variables:
+            try:
                 input_ctrl_vars_var_max = input_ctrl_vars[var]['max']
                 input_ctrl_vars_var_min = input_ctrl_vars[var]['min']
-                for i in input_ctrl_vars[var]: 
-                    assert (i in ['max','min']), "Only 'max' and 'min' inputs are accepted for control variable: " + str(var) 
-            except KeyError as error: 
-                print("<Input Validation Error> min and max values must be defined for the input variable: '"
-                        + var + "'.")
-            except AssertionError as error: 
+                for i in input_ctrl_vars[var]:
+                    assert (
+                        i in [
+                            'max', 'min']), "Only 'max' and 'min' inputs are accepted for control variable: " + str(var)
+            except KeyError as error:
+                print(
+                    "<Input Validation Error> min and max values must be defined for the input variable: '" +
+                    var +
+                    "'.")
+            except AssertionError as error:
                 print(error)
         print('out')
-        # validate special input variables 
-        # add validation here if developer adds new special input variable 
-        # polynomial 
+        # validate special input variables
+        # add validation here if developer adds new special input variable
+        # polynomial
