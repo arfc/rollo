0aef6a8f8e5a06e697fcca20cf7a5f0b290b8e6d
gwenchee <gwendolynchee95@gmail.com>
2020-11-25 22:45:43 +0800

    so far

--- realm/input_validation.py
+++ realm/input_validation.py
@@ -17,69 +17,64 @@ class InputValidation():
         schema_top_layer= {
             "type": "object", 
             "properties": {
-                "input_variables": {"type": "object"}, 
+                "control_variables": {"type": "object"}, 
                 "evaluators": {"type": "object"}, 
                 "constraints": {"type": "object"},
                 "algorithms": {"type": "object"}
             }
         }
         validate(instance=self.input, schema=schema_top_layer)
+        
+        # validate input variables 
         try:
-            input_inp_vars = self.input["input_variables"]
+            input_ctrl_vars = self.input["control_variables"]
         except KeyError as error: 
             print("<Input Validation Error> At least 1 input variable must be defined.")
         else: 
-            self.validate_inp_vars(input_inp_vars)
+            self.validate_ctrl_vars(input_ctrl_vars)
+        
+        # validate evaluators 
+        
         #self.validate_evaluators() 
         #self.validate_constraints() 
         #self.validate_algorithms()
 
-    def validate_inp_vars(self, input_inp_vars): 
+    def validate_ctrl_vars(self, input_ctrl_vars): 
         """ This function validates the 'input variables' segment of the JSON
         input file. 
         """
         # special input variables with a non-conforming input style defined in 
         # input*** (add file name that has this)
         # add to this list if a developer adds a special input variable 
-        special_inp_vars = ['polynomial']
+        special_ctrl_vars = ['polynomial']
         
         # validate regular input variables 
-        schema_inp_vars = {
+        schema_ctrl_vars = {
             "type": "object", 
             "properties": {}
         }
         variables = []
-        for var in input_inp_vars:
-            if var not in special_inp_vars: 
-                schema_inp_vars["properties"][var] = {"type": "object", 
+        for var in input_ctrl_vars:
+            if var not in special_ctrl_vars: 
+                schema_ctrl_vars["properties"][var] = {"type": "object", 
                                                             "properties": {
                                                                 "max": {"type": "number"}, 
                                                                 "min": {"type": "number"}
                                                             }}
                 variables.append(var)
-        validate(instance=input_inp_vars, schema=schema_inp_vars)
+        validate(instance=input_ctrl_vars, schema=schema_ctrl_vars)
         for var in variables: 
             try: 
-                input_inp_vars_var_max = input_inp_vars[var]['max']
-                input_inp_vars_var_min = input_inp_vars[var]['min']
+                input_ctrl_vars_var_max = input_ctrl_vars[var]['max']
+                input_ctrl_vars_var_min = input_ctrl_vars[var]['min']
+                for i in input_ctrl_vars[var]: 
+                    assert (i in ['max','min']), "Only 'max' and 'min' inputs are accepted for control variable: " + str(var) 
             except KeyError as error: 
                 print("<Input Validation Error> min and max values must be defined for the input variable: '"
                         + var + "'.")
-                
+            except AssertionError as error: 
+                print(error)
+        print('out')
         # validate special input variables 
         # add validation here if developer adds new special input variable 
         # polynomial 
-        """
-        try: 
-            
-            schema_inp_vars_polynomial = {
-                "type": "object", 
-                "properties": {
-                    "order": {"type": "number"}, 
-                    "min": {"type": "number"}, 
-                    "max": {"type": "number"}, 
-                    "above_x_axis": {"type": "boolean"}
-                    
-                }
-            }"""
-        #validate(instance=input_inp_vars['polynomial'], schema=schema_inp_vars_polynomial)
