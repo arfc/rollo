3a1e3b8e3d25d0fc65a41c13ba89284d854db811
gwenchee <gwendolynchee95@gmail.com>
2020-12-04 14:51:27 +0800

    name ind test

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,5 +1,33 @@
 import pytest
 from realm.evaluation2 import Evaluation
+from collections import OrderedDict
+
+
+def test_name_ind():
+    ev = Evaluation()
+    control_vars = ev.name_ind(
+        ind=[0.01, 1, 1, 1, 1],
+        control_dict=OrderedDict(
+            {
+                "packing_fraction": "openmc",
+                "poly_triso_0": "moltres",
+                "poly_triso_1": "moltres",
+                "poly_triso_2": "moltres",
+                "poly_triso_3": "moltres",
+            }
+        ),
+        input_evaluators=["openmc", "moltres"],
+    )
+    expected_control_vars = {
+        "openmc": {"packing_fraction": 0.01},
+        "moltres": {
+            "poly_triso_0": 1,
+            "poly_triso_1": 1,
+            "poly_triso_2": 1,
+            "poly_triso_3": 1,
+        },
+    }
+    assert control_vars == expected_control_vars
 
 
 def test_render_jinja_template_python():
