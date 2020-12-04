c9f3f653739e92121d8ee06468f56c256111eb8c
gwenchee <gwendolynchee95@gmail.com>
2020-12-04 17:56:21 +0800

    init py

--- realm/__init__.py
+++ realm/__init__.py
@@ -1,12 +1,11 @@
 from realm.algorithm import *
 from realm.backend import *
 from realm.constraints import *
-from realm.evaluation import *
+#from realm.evaluation import *
 from realm.evaluation2 import *
 from realm.executor import *
 from realm.input_validation import *
 from realm.openmc_evaluation import *
 from realm.special_variables import *
 from realm.restart import *
-
 __version__ = '0.0.1-dev'
