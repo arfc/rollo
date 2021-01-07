32ea03403f6c92733f4161ee8b56bd5a0125ab1d
gwenchee <gwendolynchee95@gmail.com>
2021-01-07 21:56:34 +0800

    finished deap operator tests

--- tests/test_deap_operators.py
+++ tests/test_deap_operators.py
@@ -1,6 +1,6 @@
 from realm.deap_operators import DeapOperators
 from deap import base, creator, tools, algorithms
-import random
+import random, copy
 
 
 def test_add_selection_operators():
@@ -53,12 +53,35 @@ def test_add_mutation_operators():
     max_list = [0.1, 1, 1, 1, 1]
 
     for mutation_dict in mutation_dict_list:
-        print(mutation_dict["operator"])
         toolbox = do.add_mutation_operators(toolbox, mutation_dict, min_list, max_list)
         mutated = toolbox.mutate(ind)
         assert "mutate" in dir(toolbox)
         assert mutated != ind
 
 
-#def test_add_mating_operators():
-    
+def test_add_mating_operators():
+    do = DeapOperators()
+    toolbox = base.Toolbox()
+    mating_dict_list = [
+        {"operator": "cxOnePoint"},
+        {"operator": "cxUniform", "indpb": 0.5},
+        {"operator": "cxBlend", "alpha": 0.5},
+    ]
+    creator.create("obj", base.Fitness, weights=(-1.0,))
+    creator.create("Ind", list, fitness=creator.obj)
+    toolbox.register("num", random.uniform, -1, 1)
+
+    def f_cycle():
+        return creator.Ind([toolbox.num(), toolbox.num(), toolbox.num(), toolbox.num()])
+
+    toolbox.register("individual", f_cycle)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    starting_pop = toolbox.population(n=2)
+    pop = copy.deepcopy(starting_pop)
+    for mating_dict in mating_dict_list:
+        new_pop = []
+        for child1, child2 in zip(pop[::2], pop[1::2]):
+            toolbox = do.add_mating_operators(toolbox, mating_dict)
+            new_pop += toolbox.mate(child1, child2)
+        assert "mate" in dir(toolbox)
+        assert len(new_pop) == len(pop)
