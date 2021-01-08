e69b70cd7436200bd67db93566c9c9623d558983
gwenchee <gwendolynchee95@gmail.com>
2021-01-08 23:33:37 +0800

    test constraints doneee

--- realm/constraints.py
+++ realm/constraints.py
@@ -5,7 +5,7 @@ import warnings
 
 class Constraints(object):
     def __init__(self, output_dict, input_constraints):
-        self.input_constraints = input_constraints
+        self.constraints = self.constraints_list(input_constraints)
         self.numbered_oup_dict = self.output_dict_numbered(output_dict)
         self.ops = {
             "<": operator.lt,
@@ -23,14 +23,42 @@ class Constraints(object):
             numbered_oup_dict[key] = i
         return numbered_oup_dict
 
+    def constraints_list(self, input_constraints):
+        constraints_list = []
+        for c in input_constraints:
+            if type(input_constraints[c]["operator"]) is list:
+                for i in range(len(input_constraints[c]["operator"])):
+                    constraints_list.append(
+                        [
+                            c,
+                            {
+                                "op": input_constraints[c]["operator"][i],
+                                "val": input_constraints[c]["constrained_val"][i],
+                            },
+                        ]
+                    )
+            else:
+                constraints_list.append(
+                    [
+                        c,
+                        {
+                            "op": input_constraints[c]["operator"],
+                            "val": input_constraints[c]["constrained_val"],
+                        },
+                    ]
+                )
+        return constraints_list
+
     def apply_constraints(self, pop):
+        new_pop = []
         for ind in pop:
-            for c in self.input_constraints:
-                index = self.numbered_oup_dict[c]
-                if self.ops[self.input_constraints[c]["operator"]](
-                    ind[index], self.input_constraints[c]["value"]
-                ):
-                    new_pop.append(ind)
-                else:
+            not_constrained = True
+            for i, c in enumerate(self.constraints):
+                index = self.numbered_oup_dict[c[0]]
+                if self.ops[c[1]["op"]](ind.output[index], c[1]["val"]):
                     pass
+                else:
+                    not_constrained = False
+            if not_constrained:
+                new_pop.append(ind)
         return new_pop
--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -13,9 +13,8 @@ test_output_dict = OrderedDict(
 )
 
 test_input_constraints = {
-    "keff": {"operator": ">=", "constrained_val": 1},
+    "keff": {"operator": [">=", "<="], "constrained_val": [1, 1.2]},
     "max_temp": {"operator": "<", "constrained_val": 500},
-    "keff": {"operator": "<=", "constrained_val": 1.2},
 }
 
 
@@ -29,3 +28,36 @@ def test_output_dict_numbered():
         "max_temp": 3,
     }
     assert numbered_oup_dict == expected_num_oup_dict
+
+
+def test_constraints_list():
+    c = Constraints(test_output_dict, test_input_constraints)
+    constraints_list = c.constraints_list(test_input_constraints)
+    expected_constraints_list = [
+        ["keff", {"op": ">=", "val": 1}],
+        ["keff", {"op": "<=", "val": 1.2}],
+        ["max_temp", {"op": "<", "val": 500}],
+    ]
+    print(constraints_list)
+    print(expected_constraints_list)
+    assert constraints_list == expected_constraints_list
+
+
+def test_apply_constraints():
+    creator.create("obj", base.Fitness, weights=(-1.0,))
+    creator.create("Ind", list, fitness=creator.obj)
+    ind1 = creator.Ind([1])
+    ind2 = creator.Ind([2])
+    ind3 = creator.Ind([3])
+    ind4 = creator.Ind([4])
+    ind5 = creator.Ind([5])
+    ind1.output = tuple([0.1, 1.1, 1, 400])
+    ind2.output = tuple([0.1, 1.1, 1, 600])
+    ind3.output = tuple([0.1, 1.4, 1, 400])
+    ind4.output = tuple([0.1, 0.9, 1, 400])
+    ind5.output = tuple([0.1, 1.15, 2, 450])
+    pop = [ind1, ind2, ind3, ind4, ind5]
+    c = Constraints(test_output_dict, test_input_constraints)
+    new_pop = c.apply_constraints(pop)
+    expected_pop = [ind1, ind5]
+    assert new_pop == expected_pop
