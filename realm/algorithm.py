24a633dab936c5abca77c214be073b64aca818b4
gwenchee <gwendolynchee95@gmail.com>
2021-01-13 01:03:43 +0800

    making algorithm.py more modular

--- realm/algorithm.py
+++ realm/algorithm.py
@@ -22,12 +22,14 @@ class Algorithm(object):
         """
         pop = self.toolbox.population(n=self.toolbox.pop_size)
         pop = self.initialize_pop(pop)
-        for g in range(ngen):
-            pop = self.apply_algorithm_ngen(pop)
+        for gen in range(ngen):
+            pop = self.apply_algorithm_ngen(pop, gen)
         print("Completed!")
         return
 
     def initialize_pop(self, pop):
+        """ Initialize population for genetic algorithm 
+        """
         for i, ind in enumerate(pop):
             ind.gen = 0
             ind.num = i
@@ -39,28 +41,14 @@ class Algorithm(object):
             ind.output = fitness
         return pop
 
-    def apply_algorithm_ngen(self, pop):
-        # apply constaint
+    def apply_algorithm_ngen(self, pop, gen):
         pop = self.constraint_obj.apply_constraints(pop)
-        # apply selection operator
-        pre_pop = self.toolbox.select(pop)
-        pop = [toolbox.clone(ind) for ind in pre_pop]
-        # extend pop length to pop_size
-        while len(pop) != self.toolbox.pop_size:
-            pop.append(toolbox.clone(random.choice(pre_pop)))
-        # apply mate operator
-        for child1, child2 in zip(pop[::2], pop[1::2]):
-            if random.random() < self.toolbox.cxpb:
-                toolbox.mate(child1, child2)
-                del child1.fitness.values, child2.fitness.values
-        # apply mutation operator
-        for mutant in pop:
-            if random.random() < self.toolbox.mutpb:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
+        pop = self.apply_selection_operator(pop)
+        pop = self.apply_mating_operator(pop)
+        pop = self.apply_mutation_operator(pop)
         # define pop's gen, ind num
         for i, ind in enumerate(pop):
-            ind.gen = g + 1
+            ind.gen = gen + 1
             ind.num = i
         # evaluate fitness of newly created population
         fitnesses = toolbox.map(toolbox.evaluate, pop)
@@ -68,4 +56,26 @@ class Algorithm(object):
         for ind, fitness in zip(pop, fitnesses):
             ind.fitness.values = (fitness[0],)
             ind.output = fitness
+        return pop
+
+    def apply_selection_operator(self, pop): 
+        pre_pop = self.toolbox.select(pop)
+        pop = [toolbox.clone(ind) for ind in pre_pop]
+        # extend pop length to pop_size
+        while len(pop) != self.toolbox.pop_size:
+            pop.append(toolbox.clone(random.choice(pre_pop)))
+        return pop
+
+    def apply_mating_operator(self, pop):
+        for child1, child2 in zip(pop[::2], pop[1::2]):
+            if random.random() < self.toolbox.cxpb:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values, child2.fitness.values
+        return pop
+
+    def apply_mutation_operator(self, pop):
+        for mutant in pop:
+            if random.random() < self.toolbox.mutpb:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
         return pop
--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -6,14 +6,22 @@ import random
 creator.create("obj", base.Fitness, weights=(-1.0,))
 creator.create("Ind", list, fitness=creator.obj)
 toolbox = base.Toolbox()
-toolbox.register("pf", random.uniform, 1, 1)
-toolbox.register("poly", random.uniform, 2, 2)
+toolbox.register("pf", random.uniform, 0, 1)
+toolbox.register("poly", random.uniform, 1, 2)
+
+
 def ind_vals():
     return creator.Ind([toolbox.pf(), toolbox.poly()])
+
+
 toolbox.register("individual", ind_vals)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+
 def evaluator_fn(ind):
     return tuple([ind[0] + ind[1], 5])
+
+
 toolbox.register("evaluate", evaluator_fn)
 test_constraints = Constraints(output_dict={}, input_constraints={})
 
@@ -23,11 +31,19 @@ def test_initialize_pop():
     pop = toolbox.population(n=5)
     pop = a.initialize_pop(pop)
     for i, ind in enumerate(pop):
-        assert ind.fitness.values[0] == 3
-        assert ind.output == tuple([3, 5])
-        assert ind == creator.Ind([1, 2])
-        assert ind.num == i 
+        assert ind.fitness.values[0] < 3
+        assert ind.fitness.values[0] > 1
+        assert ind.output[1] == 5
+        assert type(ind) is creator.Ind
+        assert ind[0] < 1
+        assert ind[0] > 0
+        assert ind[1] > 1
+        assert ind[1] < 2
+        assert ind.num == i
         assert ind.gen == 0
+
+
+def test_apply_algorithm_ngen():
 
 
 # def test_generate():
