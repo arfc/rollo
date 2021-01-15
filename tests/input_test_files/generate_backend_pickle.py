ff04d331cc4bdda7a36f7f0dceff29d81c8859de
gwenchee <gwendolynchee95@gmail.com>
2021-01-15 22:32:11 +0800

    created test pickle file

---
+++ tests/input_test_files/generate_backend_pickle.py
@@ -0,0 +1,56 @@
+import pickle
+import random
+from deap import base, creator, tools, algorithms
+import numpy as np
+
+creator.create("obj", base.Fitness, weights=(1.0,))
+creator.create("Ind", list, fitness=creator.obj)
+toolbox = base.Toolbox()
+toolbox.register("pf", random.uniform, 0, 1)
+toolbox.register("poly", random.uniform, 1, 2)
+toolbox.pop_size = 10
+toolbox.ngen = 10
+
+
+def ind_vals():
+    pf = toolbox.pf()
+    poly = toolbox.poly()
+    return creator.Ind([pf, poly, pf + poly])
+
+
+toolbox.register("individual", ind_vals)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+
+def evaluator_fn(ind):
+    return tuple([ind[0] + ind[1], 5])
+
+
+toolbox.register("evaluate", evaluator_fn)
+
+
+pop = toolbox.population(n=toolbox.pop_size)
+hof = tools.HallOfFame(maxsize=1)
+hof.update(pop)
+fitnesses = toolbox.map(toolbox.evaluate, pop)
+for ind, fitness in zip(pop, fitnesses):
+    ind.fitness.values = (fitness[0],)
+    ind.output = fitness
+invalids = [ind for ind in pop if not ind.fitness.valid]
+logbook = tools.Logbook()
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", np.mean)
+stats.register("max", np.max)
+record = stats.compile(pop)
+logbook.record(gen=toolbox.ngen, evals=len(invalids), **record)
+
+cp = dict(
+    population=pop,
+    generation=0,
+    halloffame=hof,
+    logbook=logbook,
+    rndstate=random.getstate(),
+)
+checkpoint_file = "square_checkpoint.pkl"
+with open(checkpoint_file, "wb") as cp_file:
+    pickle.dump(cp, cp_file)
