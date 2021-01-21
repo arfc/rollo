f4b0b4c400628d16523acdef15ee8362ba8572bc
gwenchee <gwendolynchee95@gmail.com>
2021-01-22 00:21:01 +0800

    min max in cli

--- realm/__main__.py
+++ realm/__main__.py
@@ -1,29 +1,31 @@
 import sys, getopt
-from deap import creator, base
 
 sys.path.insert(1, "realm/")
 from realm import executor
+from deap import creator, base
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
-    print(argv)
+    msg = "python realm -i <inputfile> -c <checkpoint file> -p <min or max obj>"
     try:
-        opts, args = getopt.getopt(argv, "i:m:c:")
+        opts, args= getopt.getopt(argv, "i:p:c:")
     except getopt.GetoptError:
-        print("python realm -i <inputfile>")
+        print("To run realm:")
+        print(msg)
+    wt_dict = {"min":-1.0, "max":+1.0}
     opts_dict = {}
     for opt, arg in opts:
         opts_dict[opt] = arg
-    if "-h" in opts_dict:
-        print("python realm -i <inputfile>")
-    elif "-i" in opts_dict:
+    if "-i" in opts_dict and "-p" in opts_dict:
+        creator.create("obj", base.Fitness, weights=(wt_dict[opts_dict["-p"]],))
+        creator.create("Ind", list, fitness=creator.obj)
         if "-c" in opts_dict:
             new_run = executor.Executor(
                 input_file=opts_dict["-i"], checkpoint_file=opts_dict["-c"]
             )
         else:
             new_run = executor.Executor(input_file=opts_dict["-i"])
-    creator.create("obj", base.Fitness, weights=(-1.0,))
-    creator.create("Ind", list, fitness=creator.obj)
-    if "-i" in opts_dict:
         new_run.execute()
+    else:
+        print("To run realm:")
+        print(msg)
