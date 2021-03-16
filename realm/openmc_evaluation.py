8f3f9ba85b464936834cd98250b87d9c7f71414a
gwenchee <gwendolynchee95@gmail.com>
2021-03-17 01:48:15 +0800

    prints

--- realm/openmc_evaluation.py
+++ realm/openmc_evaluation.py
@@ -1,6 +1,6 @@
 import glob
 import openmc
-
+import time
 
 class OpenMCEvaluation:
     """This class evaluates openmc output files.
@@ -13,8 +13,13 @@ class OpenMCEvaluation:
 
     def evaluate_keff(self):
         """This function analyzes the openmc output file"""
+        start = time.time()
+        print("eval_keff start", start)
         for file in glob.glob("statepoint*"):
             h5file = file
+        print("in_1", time.time()-start)
         sp = openmc.StatePoint(h5file)
+        print("in_2", time.time()-start)
         keff = sp.k_combined.nominal_value
+        print("in_3", time.time()-start)
         return keff
