f472fa863fcb38d27f9492ad5136e6fea9cbce80
gwenchee <gwendolynchee95@gmail.com>
2021-05-06 22:46:31 +0800

    rollo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## rollo (Reactor evOLutionary aLgorithm Optimizer)
 Repository to hold the framework and code to produce generative reactor designs.
 
-python realm -i <input file> -c <checkpoint file>
+python rollo -i <input file> -c <checkpoint file>
 
 # To build documentation 
 cd docs 
--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gwenchee", 
+    name="gwenchee",
     version="0.0.1",
     author="Gwendolyn J.Y. Chee",
     author_email="gwendolynchee95@gmail.com",
     description="Generative Reactor Designs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/arfc/generative_reactor_designs",
+    url="https://github.com/arfc/rollo",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD 3-Clause License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
