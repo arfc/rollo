6bfb891437ad8417929024215092a58c64ab6b00
PEP 8 Speaks <pep8speaks@gmail.com>
2021-05-19 21:55:23 +0530

    Fix PEP 8 errors in examples/fhr-slab/openmcinp.py

--- examples/fhr-slab/openmcinp.py
+++ examples/fhr-slab/openmcinp.py
@@ -1,10 +1,10 @@
+from constants import *
 import openmc
 import numpy as np
 from numpy import sin, cos, tan, pi
 import sys
 
 sys.path.insert(1, "../")
-from constants import *
 
 # Templating
 total_pf = 0.0979
