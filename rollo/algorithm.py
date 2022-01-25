117314ef6c1ae25ebd2f587f7c07ec23d7bbe4db
gwenchee <gwendolynchee95@gmail.com>
2022-01-25 11:07:43 -0600

    list fitnesses , instead of map type

--- rollo/algorithm.py
+++ rollo/algorithm.py
@@ -131,7 +131,7 @@ class Algorithm(object):
         # evaluate fitness values of initial pop
         invalids = [ind for ind in pop if not ind.fitness.valid]
         copy_invalids = [self.toolbox.clone(ind) for ind in invalids]
-        fitnesses = self.toolbox.map(self.toolbox.evaluate, pop)
+        fitnesses = list(self.toolbox.map(self.toolbox.evaluate, pop))
         # assign fitness values to individuals
         for ind, fitness in zip(pop, fitnesses):
             fitness_vals = []
@@ -170,7 +170,7 @@ class Algorithm(object):
         # evaluate fitness of newly created pop for inds with invalid fitness
         invalids = [ind for ind in pop if not ind.fitness.valid]
         copy_invalids = [self.toolbox.clone(ind) for ind in invalids]
-        fitnesses = self.toolbox.map(self.toolbox.evaluate, list(invalids))
+        fitnesses = list(self.toolbox.map(self.toolbox.evaluate, list(invalids)))
         # assign fitness values to individuals
         for ind, fitness in zip(invalids, fitnesses):
             fitness_vals = []
