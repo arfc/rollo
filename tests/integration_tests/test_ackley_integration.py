12bc3194ef4cc61cbc2a8278b62c1a6ab11f3c22
gwenchee <gwendolynchee95@gmail.com>
2022-03-04 18:41:46 -0600

    pep8

--- tests/integration_tests/test_ackley_integration.py
+++ tests/integration_tests/test_ackley_integration.py
@@ -5,8 +5,11 @@ The minimum value is 0 at (0,0).
 We use it as a performance test for single-objective optimization.
 """
 
-import os, subprocess, pickle 
+import os
+import subprocess
+import pickle
 from deap import base, creator, tools, algorithms
+
 
 def test_ackley_integration():
 
@@ -14,10 +17,12 @@ def test_ackley_integration():
     creator.create("Ind", list, fitness=creator.obj)
 
     os.chdir("./input_test_files")
-    subprocess.call("python ../../../rollo -i input_test_ackley.json", shell=True)
+    subprocess.call(
+        "python ../../../rollo -i input_test_ackley.json",
+        shell=True)
     os.chdir("../")
     with open("./input_test_files/checkpoint.pkl", "rb") as cp_file:
         cp = pickle.load(cp_file)
 
     logbook = cp["all"]
-    assert min(logbook["outputs"][-1])[0] < 0.1 
+    assert min(logbook["outputs"][-1])[0] < 0.1
--- tests/integration_tests/test_binh_korn.py
+++ tests/integration_tests/test_binh_korn.py
@@ -1,30 +1,38 @@
 """Integration Test to validate the Evolutionary Algorithm.
-The Binh and Korn function is a two-objective function. 
-[1]T. T. Binh and U. Korn, “MOBES: A multiobjective evolution strategy 
-for constrained optimization problems,” in The third international 
+The Binh and Korn function is a two-objective function.
+[1]T. T. Binh and U. Korn, “MOBES: A multiobjective evolution strategy
+for constrained optimization problems,” in The third international
 conference on genetic algorithms (Mendel 97), 1997, vol. 25, p. 27.
 We use it as a performance test for two-objective optimization.
 """
 
-import os, subprocess, pickle 
+import os
+import subprocess
+import pickle
 from deap import base, creator, tools
 import deap.benchmarks.tools as bt
 import numpy as np
 
+
 def test_binh_hypervolume():
 
-    creator.create("obj", base.Fitness, weights=(-1.0,-1.0))
+    creator.create("obj", base.Fitness, weights=(-1.0, -1.0))
     creator.create("Ind", list, fitness=creator.obj)
 
     os.chdir("./input_test_files")
-    subprocess.call("python ../../../rollo -i input_test_binh.json", shell=True)
+    subprocess.call(
+        "python ../../../rollo -i input_test_binh.json",
+        shell=True)
     os.chdir("../")
     with open("./input_test_files/checkpoint.pkl", "rb") as cp_file:
         cp = pickle.load(cp_file)
 
     results = cp["all"]
     final_pop = results["populations"][-1]
-    non_dom = tools.sortNondominated(final_pop, k=len(final_pop), first_front_only=True)[0]
-    ref = np.array([120,50])
+    non_dom = tools.sortNondominated(
+        final_pop,
+        k=len(final_pop),
+        first_front_only=True)[0]
+    ref = np.array([120, 50])
     hypervol = bt.hypervolume(non_dom, ref)
-    assert hypervol > 4200 
+    assert hypervol > 4200
