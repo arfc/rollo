418364c2ac42fb8210d773da1865d10cb338d95d
gwenchee <gwendolynchee95@gmail.com>
2022-03-08 11:07:32 -0600

    add fn doc string

--- rollo/evaluation.py
+++ rollo/evaluation.py
@@ -141,6 +141,21 @@ class Evaluation:
         return eval_function
 
     def solver_order(self, input_evaluators):
+        """Returns a list with solver name at its order index
+
+        Parameters
+        ----------
+        input_evaluators : dict
+            evaluators sub-dictionary from input file
+
+        Returns
+        -------
+        list
+            list with solver name at its order index
+            e.g. if openmc solver order = 0 and moltres solver order = 1, the
+            returned list looks like ["openmc", "moltres"]
+
+        """
         order = [None] * len(input_evaluators)
         for solver in input_evaluators:
             order[input_evaluators[solver]["order"]] = solver
